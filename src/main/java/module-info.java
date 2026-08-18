ef522d4e7c3c4d2092c7bdadea6c098f2678da61
teokj <141907128+teokj@users.noreply.github.com>
2026-08-18 10:15:00 +0800

    Set up initial JavaFX project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.kengjer.applytrack {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.kengjer.applytrack to javafx.fxml;
+    exports org.kengjer.applytrack;
+}
