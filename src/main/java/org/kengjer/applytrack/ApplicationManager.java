1d15007c67006f4bb0a00bc0296a2bc8f63d1f53
teokj <141907128+teokj@users.noreply.github.com>
2026-08-23 14:05:03 +0800

    Implement application details and deletion

--- src/main/java/org/kengjer/applytrack/ApplicationManager.java
+++ src/main/java/org/kengjer/applytrack/ApplicationManager.java
@@ -33,4 +33,8 @@ public class ApplicationManager {
     public List<JobApplication> getApplications() {
         return applications;
     }
+
+    public void removeApplication(JobApplication application) {
+        applications.remove(application);
+    }
 }
